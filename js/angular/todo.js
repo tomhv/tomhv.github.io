472c4501bb66b4ad1a117b3dbf8b21fd80e86d94
Tom Haskins-Vaughan <tom@tomhv.uk>
2015-11-04 08:57:03 -0500

    Adding default todos

--- js/angular/todo.js
+++ js/angular/todo.js
@@ -1,4 +1,10 @@
 angular.module('myApp', [])
     .controller('TodoCtrl', function($scope) {
         $scope.totalTodos = 10;
+        
+        // initialize with some existing todos
+        $scope.todos = [
+            { text: 'Buy cat food', done: false },
+            { text: 'Eat candy', done: false }
+        ];
     });
